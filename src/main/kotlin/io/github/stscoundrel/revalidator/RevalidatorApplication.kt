ad2b9fa78d2d36920a477ecb9543d72c151a5b8f
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-04-10 13:43:52 +0300

    Add Sprint Boot / Kotlin setup

---
+++ src/main/kotlin/io/github/stscoundrel/revalidator/RevalidatorApplication.kt
@@ -0,0 +1,11 @@
+package io.github.stscoundrel.revalidator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RevalidatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<RevalidatorApplication>(*args)
+}
