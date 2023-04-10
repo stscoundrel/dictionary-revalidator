ec72cd73667d4b634187c2343315e9e5cadbf8cc
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-04-10 13:45:34 +0300

    Add controller endpoints for individual dictionary revalidations

---
+++ src/main/kotlin/io/github/stscoundrel/revalidator/controller/RevalidateController.kt
@@ -0,0 +1,54 @@
+package io.github.stscoundrel.revalidator.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+import io.github.stscoundrel.revalidator.service.RevalidatorService
+import io.github.stscoundrel.revalidator.enum.DictionaryType
+
+
+data class RevalidationResponse(val statuses: Map<DictionaryType, Boolean>)
+
+@RestController
+class RevalidateController(val revalidatorService: RevalidatorService) {
+    @GetMapping("/old-norse")
+    fun oldNorse(): RevalidationResponse {
+        revalidatorService.revalidateOldNorse()
+        return RevalidationResponse(mapOf(DictionaryType.OLD_NORSE to true))
+    }
+
+    @GetMapping("/old-icelandic")
+    fun oldIcelandic(): RevalidationResponse {
+        revalidatorService.revalidateOldIcelandic()
+        return RevalidationResponse(mapOf(DictionaryType.OLD_ICELANDIC to true))
+    }
+
+    @GetMapping("/old-swedish")
+    fun oldSwedish(): RevalidationResponse {
+        revalidatorService.revalidateOldSwedish()
+        return RevalidationResponse(mapOf(DictionaryType.OLD_SWEDISH to true))
+    }
+
+    @GetMapping("/old-norwegian")
+    fun oldNorwegian(): RevalidationResponse {
+        revalidatorService.revalidateOldNorwegian()
+        return RevalidationResponse(mapOf(DictionaryType.OLD_NORWEGIAN to true))
+    }
+
+    @GetMapping("/")
+    fun all(): RevalidationResponse {
+        revalidatorService.revalidateOldIcelandic()
+        revalidatorService.revalidateOldNorse()
+        revalidatorService.revalidateOldNorwegian()
+        revalidatorService.revalidateOldSwedish()
+
+
+        return RevalidationResponse(mapOf(
+            DictionaryType.OLD_NORSE to true,
+            DictionaryType.OLD_ICELANDIC to true,
+            DictionaryType.OLD_SWEDISH to true,
+            DictionaryType.OLD_NORWEGIAN to true,
+        ))
+    }
+}
---
+++ src/main/kotlin/io/github/stscoundrel/revalidator/service/RevalidatorService.kt
@@ -0,0 +1,50 @@
+package io.github.stscoundrel.revalidator.service
+
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.collections.MutableMap
+import org.springframework.stereotype.Service
+
+import io.github.stscoundrel.revalidator.enum.DictionaryType
+import io.github.stscoundrel.revalidator.revalidators.Revalidator
+import io.github.stscoundrel.revalidator.repository.RevalidatorConfigRepository
+import io.github.stscoundrel.revalidator.service.HTTPClient
+
+@Service
+class RevalidatorService(val configRepository: RevalidatorConfigRepository, val httpClient: HTTPClient) {
+    val revalidators: MutableMap<DictionaryType, Revalidator?> = mutableMapOf(
+        DictionaryType.OLD_NORSE to null,
+        DictionaryType.OLD_ICELANDIC to null,
+        DictionaryType.OLD_SWEDISH to null,
+        DictionaryType.OLD_NORWEGIAN to null,
+    )
+
+    private fun getDictionaryRevalidator(dictionaryType: DictionaryType): Revalidator {
+        if (revalidators[dictionaryType] == null) {
+            val config = configRepository.getConfig(dictionaryType)
+            revalidators[dictionaryType] = Revalidator(config, httpClient)
+        }
+
+        return revalidators[dictionaryType]!!
+    }
+
+    fun revalidateOldNorse() {
+        val dictionary = getDictionaryRevalidator(DictionaryType.OLD_NORSE)
+        dictionary.revalidate()
+    }
+
+    fun revalidateOldIcelandic() {
+        val dictionary = getDictionaryRevalidator(DictionaryType.OLD_ICELANDIC)
+        dictionary.revalidate()
+    }
+
+    fun revalidateOldSwedish() {
+        val dictionary = getDictionaryRevalidator(DictionaryType.OLD_SWEDISH)
+        dictionary.revalidate()
+    }
+
+    fun revalidateOldNorwegian() {
+        val dictionary = getDictionaryRevalidator(DictionaryType.OLD_NORWEGIAN)
+        dictionary.revalidate()
+    }
+}
