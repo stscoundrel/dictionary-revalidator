56f08d9cc310b2d57193813254e1255b320d50e7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-04-21 16:00:29 +0300

    Expose logs via /api/logs/ endpoint

--- src/main/kotlin/io/github/stscoundrel/revalidator/repository/LogRepository.kt
+++ src/main/kotlin/io/github/stscoundrel/revalidator/repository/LogRepository.kt
@@ -1,6 +1,9 @@
 package io.github.stscoundrel.revalidator.repository
 
 import io.github.stscoundrel.revalidator.entities.Log
+import io.github.stscoundrel.revalidator.enums.DictionaryType
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LogRepository : JpaRepository<Log, Long>
+interface LogRepository : JpaRepository<Log, Long> {
+    fun findByDictionaryType(dictionaryType: DictionaryType): List<Log>
+}
